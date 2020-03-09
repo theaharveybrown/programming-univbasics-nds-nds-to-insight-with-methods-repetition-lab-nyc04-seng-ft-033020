50a38697b0c6dcf0882063a19b535a463ea2b4cc
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-09 14:23:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,7 +46,7 @@ def total_gross(source)
     total += directors[name]
     i += 1
   end
-  total 
+  total
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
